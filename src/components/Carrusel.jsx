a58d5049989d552f53019e7d80c5441b8d16702a
Alejandro <alejandro.arafi@gmail.com>
2024-11-21 20:26:41 -0300

    carruselok

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -3,8 +3,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/autoplay";
-import "swiper/css/navigation";
-import "swiper/css/pagination";
+
 import "./carrusel.css";
 
 const images = ["/images/c1.jpg", "/images/c2.jpg", "/images/c3.jpg"];
