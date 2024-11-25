77f1930f637ed8de2574076166eb6e84ae5185db
Alejandro <alejandro.arafi@gmail.com>
2024-11-25 17:05:54 -0300

    form responsive

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{astro,js,jsx,ts,tsx}"], // Para asegurar que Tailwind escanee los archivos correctos
-  darkMode: "media", // o 'media' o 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
