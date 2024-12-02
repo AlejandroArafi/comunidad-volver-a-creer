bb45587ab06ba571f0f239ef63a376fb150655b4
Alejandro <alejandro.arafi@gmail.com>
2024-12-01 21:12:10 -0300

    form ok

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import emailjs from "emailjs-com";
-import "./ContactForm.css"; // Importa el archivo CSS
+import "./ContactForm.css";
 
 const ContactForm = () => {
   const [formData, setFormData] = useState({
